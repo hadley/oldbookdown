cd073d6117629f40b8666a8993833dd84bb6a453
hadley <h.wickham@gmail.com>
2014-05-29 11:52:58 -0500

    Bound pdf screenshots to max width

--- R/png.R
+++ R/png.R
@@ -4,10 +4,12 @@ embed_png <- function(path, dpi = NULL) {
   if (!is.null(dpi)) meta$dpi <- rep(dpi, 2)
 
   if (identical(doc_type(), "latex")) {
+    width <- round(meta$dim[1] / meta$dpi[1], 2)
+    if (width > 4.35) width <- 4.35
+
     knitr::asis_output(paste0(
       "\\includegraphics[",
-      "width=", round(meta$dim[1] / meta$dpi[1], 2), "in,",
-      "height=", round(meta$dim[2] / meta$dpi[2], 2), "in",
+      "width=", width, "in",
       "]{", path, "}"
     ))
   } else {
