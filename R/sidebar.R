fdf3546f80e1db0967d1c136ad4d5666fedeca19
hadley <h.wickham@gmail.com>
2014-05-29 11:08:29 -0500

    New strategy for sidebars in latex

--- R/sidebar.R
+++ R/sidebar.R
@@ -4,16 +4,16 @@ knit_print.knit_asis <- function(x, ...) x
 #' @export
 begin_sidebar <- function(title = NULL) {
   if (identical(doc_type(), "latex")) {
-    # knitr::asis_output("\\begin{sidebar}")
+    knitr::asis_output(paste0("\\begin{SIDEBAR}", title, "\\end{SIDEBAR}\n"))
   } else {
-    knitr::asis_output("<div class = 'well'>\n")
+    knitr::asis_output(paste0("<div class = 'sidebar'><h3>", title, "</h3>\n\n"))
   }
 }
 
 #' @export
 end_sidebar <- function() {
   if (identical(doc_type(), "latex")) {
-    # knitr::asis_output("\\end{sidebar}")
+    knitr::asis_output("\\begin{ENDSIDEBAR}\\end{ENDSIDEBAR}\n")
   } else {
     knitr::asis_output("</div>\n")
   }
